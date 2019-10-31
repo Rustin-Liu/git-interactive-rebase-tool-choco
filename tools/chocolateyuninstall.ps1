0ef1bd35bc5b09cd8137009b27c6b6878e9271ca
Rustin-Liu <rustin.liu@gmail.com>
2019-10-31 19:23:04 +0800

    init

---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,50 @@
+﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+
+## NOTE: In 80-90% of the cases (95% with licensed versions due to Package Synchronizer and other enhancements),
+## AutoUninstaller should be able to detect and handle registry uninstalls without a chocolateyUninstall.ps1.
+## See https://chocolatey.org/docs/commands-uninstall
+## and https://chocolatey.org/docs/helpers-uninstall-chocolatey-package
+
+## If this is an MSI, ensure 'softwareName' is appropriate, then clean up comments and you are done.
+## If this is an exe, change fileType, silentArgs, and validExitCodes
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$installDir = Join-Path $(Get-ToolsLocation) $env:ChocolateyPackageName
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'git-interactive-rebase-tool*'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  ZipFileName   = 'git-interactive-rebase-tool-1.1.0-x86_64-pc-windows-msvc.zip'
+}
+
+$uninstalled = $false
+# Get-UninstallRegistryKey is new to 0.9.10, if supporting 0.9.9.x and below,
+# take a dependency on "chocolatey-core.extension" in your nuspec file.
+# This is only a fuzzy search if $softwareName includes '*'. Otherwise it is 
+# exact. In the case of versions in key names, we recommend removing the version
+# and using '*'.
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % { 
+    Remove-Item $installDir
+    Uninstall-ChocolateyZipPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
+
+## OTHER POWERSHELL FUNCTIONS
+## https://chocolatey.org/docs/helpers-reference
+#Uninstall-ChocolateyZipPackage $packageName # Only necessary if you did not unpack to package directory - see https://chocolatey.org/docs/helpers-uninstall-chocolatey-zip-package
+#Uninstall-ChocolateyEnvironmentVariable # 0.9.10+ - https://chocolatey.org/docs/helpers-uninstall-chocolatey-environment-variable 
+#Uninstall-BinFile # Only needed if you used Install-BinFile - see https://chocolatey.org/docs/helpers-uninstall-bin-file
+## Remove any shortcuts you added in the install script.
+
